8cc161c053ded61331f45f1c4bbc8eddcd28d6b4
Lucas <lucasbohorquez30@gmail.com>
2025-12-21 12:05:40 -0500

    codigo subido2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
